37c6930b5400150dbf3f7136b48a1be32885ef90
suyongxiang <you@example.com>
2023-12-03 15:52:12 +0800

    COME GET YA CODE

--- disposable.py
+++ disposable.py
@@ -1,7 +1,10 @@
 print("hello little child or man i dont really know")
 height = float(input("ur height in cm please"))
 if height >= 120:
-    print("you can ride")
-    
+    age = float(input("your age please"))
+    if age >= 16:
+        print("thou shall ride in this holy cart")
+    else:
+        print("go wait for a few more years you baby")
 else:
     print("go grow taller")
