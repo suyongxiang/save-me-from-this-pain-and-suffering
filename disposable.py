30a50235ad496a04aae115cb945a80623fb58125
suyongxiang <you@example.com>
2023-12-03 15:39:53 +0800

    TES TEST

--- disposable.py
+++ disposable.py
@@ -1,20 +1,7 @@
-
-discountentry = 9/10
-discountshoe = 2/4
-price = 0
-soccer = input("u want soccer? yes or no")
-amount = float(input("amount of balls"))
-if soccer == "yes":
-    price = 5*amount
-entry = input("u want entry? yes or no")
-amounts = float(input("amount of students"))
-amountd = float(input("amount of NON students"))
-if entry == "yes":
-    price = price + 10*discountentry*amounts + 10*amountd   
-shoe = input("u want shoe? yes or no")
-amount = float(input("amount of shoes"))
-amounts = float(input("amount of students"))
-amountd = float(input("amount of NON students"))
-if shoe == "yes":
-    price = price + 6*discountshoe*amounts + 6*amountd
-print ("price is", round(price,2))
+print("hello little child or man i dont really know")
+height = float(input("ur height in cm please"))
+if height >= 120:
+    print("you can ride")
+    
+else:
+    print("go grow taller")
