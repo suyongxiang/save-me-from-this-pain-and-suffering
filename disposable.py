4c489fe118151364ddbaa0ad889cab665c4189be
suyongxiang <you@example.com>
2023-12-03 15:58:48 +0800

    jhfgfgg

--- disposable.py
+++ disposable.py
@@ -2,9 +2,11 @@ print("hello little child or man i dont really know")
 height = float(input("ur height in cm please"))
 if height >= 120:
     age = float(input("your age please"))
-    if age >= 16:
-        print("thou shall ride in this holy cart")
+    if age > 18:
+        print("thou shall ride in this holy cart for a very expensive price of 12")
+    elif age>12 and age<18:
+        print("congrats we just charge you like 7 ")
     else:
-        print("go wait for a few more years you baby")
+        print("hello you young child come into this ride for 5")
 else:
     print("go grow taller")
